81d49445e9df02590dae9add64aeedd532c2cd7f
Gallefray <GallefrayVBS@aol.com>
2013-07-01 11:18:00 +0100

    Got pixels drawing on the screen thanks to some SDL wizardry, about to start on the rect algorithm

--- QL_LANG/QL.c
+++ QL_LANG/QL.c
@@ -4,21 +4,20 @@
 #include <stdint.h> // this allows Sint16 and stuff like that
 #include <time.h>   // Gives access to system time (used to set the seed :D )
 #include <SDL/SDL.h>// SDL library
-#include <SDL/SDL_gfxPrimitives.h> // SDL gfx library
-#include <assert.h> // Checks if things exist ;)
+#include <SDL/SDL_gfxPrimitives.h> // SDL gfx library. Yeah, so what. I'm using SDL_gfx. And?
 
 // Defines:
 #define true 1
 #define false 0
 #define bool int
 
-uint16_t red, green, blue, alpha;
+//uint16_t red, green, blue, alpha;
 
 SDL_Event event;
 const SDL_VideoInfo* info = NULL;
 SDL_Surface *screen;
 
-
+int colour;
 // Yes, yes. The library structure is a mess... I will reorganise it when the core libs are in :P
 
 
@@ -39,8 +38,7 @@ bool initWindow(int height, int width, bool fullscreen, char *name)
 
 	if (fullscreen == true)
 	{
-		screen = SDL_SetVideoMode(height, width, bitPerPixel, SDL_DOUBLEBUF |SDL_FULLSCREEN);// == -1)//SDL_HWSURFACE | SDL_DOUBLEBUF |SDL_FULLSCREEN) == -1)
-		//{
+		screen = SDL_SetVideoMode(height, width, bitPerPixel, SDL_DOUBLEBUF |SDL_FULLSCREEN);
 		if ( screen == NULL ) 
 		{
 			
@@ -48,47 +46,26 @@ bool initWindow(int height, int width, bool fullscreen, char *name)
 		    printf("Couldn't set %dx%d video mode or mode not supported: %s\n", height, width, SDL_GetError());
 		    exit(1);
 		}
-		//	return false;
-		//}
 	}
 	else
 	{
-		screen = SDL_SetVideoMode(height, width, bitPerPixel, SDL_DOUBLEBUF); //== -1)//SDL_HWSURFACE | SDL_DOUBLEBUF) == -1)
-		//{
+		screen = SDL_SetVideoMode(height, width, bitPerPixel, SDL_DOUBLEBUF);
 		if ( screen == NULL ) 
 		{
 		    fprintf(stderr, "Couldn't set %dx%d video mode or mode not supported: %s\n", height, width, SDL_GetError());
 		    printf("Couldn't set %dx%d video mode or mode not supported: %s\n", height, width, SDL_GetError());
 		    exit(1);
 		}
-		//	return false;
-		//}
-		// else
-		// {
-		// 	printf("screen data - width: %d, height: %d, BBP: %d \n", width, height, bitPerPixel);	// The SDL surface is def being created!! D:
-		// }
 	}
-	//assert(screen != NULL); // Error above here!!!!
 
+	colour = SDL_MapRGBA(screen->format, 255, 255, 255, 255); // Default colour is white.
 	SDL_WM_SetCaption(name, NULL ); // set the caption, as per the new 4th arg!
-
-	// SDL_WM_GrabInput(SDL_GRAB_ON);  // This makes the window grab all input :P
-	// NEVER. NEVER SET THAT UNLESS KYBOARD INPUT HAS BEEN ENABLED, AND YOU HAVE IT TOP CATCH THE ESC BUTTON. OR YOUR PC WILL DIE.
-	// It *was* pretty funny, though xD
-	if ( screen == NULL ) 
-	{
-		
-	    fprintf(stderr, "Couldn't set %dx%d video mode or mode not supported: %s\n", height, width, SDL_GetError());
-	    printf("Couldn't set %dx%d video mode or mode not supported: %s\n", height, width, SDL_GetError());
-	    exit(1);
-	}
 	return true;
 }
 
 int randomNum(int min, int max)
 {	
-	//(rand()%max);
-	return min+(rand()%((max+1)-min)); // inclusive :D
+	return min + (rand() % ((max + 1) - min)); // inclusive :D
 }
 
 bool rectRectCollision(int Ax, int Ay, int Ah, int Aw, int Bx, int By, int Bh, int Bw)
@@ -103,13 +80,29 @@ bool rectRectCollision(int Ax, int Ay, int Ah, int Aw, int Bx, int By, int Bh, i
 	}
 }
 
-int setColour(uint16_t r, uint16_t g, uint16_t b, uint16_t a)
+int setColour(int r, int g, int b, int a)
 {
-	red = r;
-	green = g;
-	blue = b;
-	alpha = a;
+	colour = SDL_MapRGBA(screen->format, r, g, b, a);
 	return 0;
+}
+
+void line(int xi, int yi, int xii, int yii) // This needs to be changed to int later on.
+{
+	int xdelta = xi - xii;
+	int ydelta = yi - yii;
+	if (xdelta > ydelta)
+	{
+		SDL_LockSurface(screen);
+		
+		SDL_UnlockSurface(screen);
+	}
+	else if (ydelta < xdelta)
+	{
+		SDL_LockSurface(screen);
+		
+		SDL_UnlockSurface(screen);
+	}
+
 }
 
 int rect(char *type, int16_t x, int16_t y, int16_t w, int16_t h)
@@ -117,11 +110,12 @@ int rect(char *type, int16_t x, int16_t y, int16_t w, int16_t h)
 	if (strcmp(type, "fill") == 0)
 	{
 		//float 
-		boxRGBA(screen, x, y, x+w, y+h, red, green, blue, alpha);
+		//	boxRGBA(screen, x, y, x+w, y+h, red, green, blue, alpha);
+
 	}
 	else if (strcmp(type, "line") == 0)
 	{
-		rectangleRGBA(screen, x, y, x+w, y+h, red, green, blue, alpha);	
+	//	rectangleRGBA(screen, x, y, x+w, y+h, red, green, blue, alpha);	
 	}
 	else
 	{
@@ -150,7 +144,7 @@ int update(int FPS)
 	#ifdef EMSCRIPTEN
 	emscripten_set_main_loop(main, FPS, 0);
 	#else
-
+	
 	SDL_Flip(screen);
 	return 0;
 	#endif
@@ -158,26 +152,36 @@ int update(int FPS)
 
 int grabKeyInput()
 {
-	while( SDL_PollEvent( &event ) ){
-	    /* We are only worried about SDL_KEYDOWN and SDL_KEYUP events */
-	    switch( event.type ){
-	     	case SDL_KEYDOWN:
-	       		printf( "Key press detected\n" );
-	        	break;
+	// while( SDL_PollEvent( &event ) ){
+	//     /* We are only worried about SDL_KEYDOWN and SDL_KEYUP events */
+	//     switch( event.type ){
+	//      	case SDL_KEYDOWN:
+	//      		switch(keyevent.key.keysym.sym)
+	//      		{
+	// 	       		printf( "Key press detected\n" );
+	// 	       	}
+	//         	break;
 
-	      	case SDL_KEYUP:
-	        	printf( "Key release detected\n" );
-	        	break;
+	//       	case SDL_KEYUP:
+	//         	printf( "Key release detected\n" );
+	//         	break;
 
-	        case SDL_QUIT:
-	        	// put something here to deInit openGL and SDL and quit :D
-                break;
-	      	default:
-	        	break;
-	    }
-	}
+	//         case SDL_QUIT:
+	//         	// put something here to deInit openGL and SDL and quit :D
+ //                break;
+	//       	default:
+	//         	break;
+	//     }
+	// }
     return 0;
 }
+
+// int grabInput()
+// {
+	// SDL_WM_GrabInput(SDL_GRAB_ON);  // This makes the window grab all input :P
+	// NEVER. NEVER SET THAT UNLESS KYBOARD INPUT HAS BEEN ENABLED, AND YOU HAVE IT TOP CATCH THE ESC BUTTON. OR YOUR PC WILL DIE.
+	// It *was* pretty funny, though xD
+// }
 
 // int grabMouseInput(float mouseX, float mouseY)
 // {
@@ -205,3 +209,36 @@ int grabKeyInput()
 //         }
 //     }   
 // }
+
+void pixel(int x, int y) // Blatantly copied from the SDL example
+{
+	int bpp = screen->format->BytesPerPixel;
+    /* Here p is the address to the pixel we want to set */
+    Uint8 *p = (Uint8 *)screen->pixels + y * screen->pitch + x * bpp;
+
+    switch(bpp) {
+    case 1:
+        *p = colour;
+        break;
+
+    case 2:
+        *(Uint16 *)p = colour;
+        break;
+
+    case 3:
+        if(SDL_BYTEORDER == SDL_BIG_ENDIAN) {
+            p[0] = (colour >> 16) & 0xff;
+            p[1] = (colour >> 8) & 0xff;
+            p[2] = colour & 0xff;
+        } else {
+            p[0] = colour & 0xff;
+            p[1] = (colour >> 8) & 0xff;
+            p[2] = (colour >> 16) & 0xff;
+        }
+        break;
+
+    case 4:
+        *(Uint32 *)p = colour;
+        break;
+    }
+}
--- QL_LANG/QL.h
+++ QL_LANG/QL.h
@@ -4,24 +4,20 @@
 #include <SDL/SDL.h>
 #include <SDL/SDL_gfxPrimitives.h>
 #include <time.h>
-#include <assert.h>
 
 #define true 1
 #define false 0
 #define bool int
 
-uint16_t red, green, blue, alpha;
-
-SDL_Surface *screen;
+//uint16_t red, green, blue, alpha;
 
 // Function definitions:
-int randomNum(int min, int max);
 bool initWindow(int height, int width, bool fullscreen, char *name);
+int randomNum(int min, int max);
 bool rectRectCollision(int Ax, int Ay, int Ah, int Aw, int Bx, int By, int Bh, int Bw);
-int setColour(uint16_t r, uint16_t g, uint16_t b, uint16_t a);
-// bool keyInput(char key);
+int setColour(int r, int g, int b, int a);
+void line(int xi, int yi, int xii, int yii);
 int rect(char *type, int16_t x, int16_t y, int16_t w, int16_t h);
-// int setLineWidth(int width);
-// int clear();
 int update(int FPS);
 int grabKeyInput();
+void pixel(int x, int y); // Blatantly copied from the SDL example
